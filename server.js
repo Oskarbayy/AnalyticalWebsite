6e41049fc1c7ddb2851fa7bbc6bf5301c4a18e86
Oskar Hansen <Oskarbayy@gmail.com>
2024-04-30 09:42:02 +0200

    Testing

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("Server!")
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server");
+});
+
+
+mongoose.connect("mongodb+srv://oskarbayy:MvbNfgvsAkk1TNy9@petsdb.dyezw73.mongodb.net/Node-API?retryWrites=true&w=majority&appName=PetsDB")
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(4000, () => {
+        console.log("Server is running on port 4000");
+    });
+})
+.catch(() => {
+    console.log("Connection Failed!");
+})
